4f83e3c4b2d3b21b1eb379e7fc68e029576be941
Andrew <asoborev@mail.ru>
2026-05-16 23:04:59 +0300

    рефакторинг task_service

--- internal/service/task_service.go
+++ internal/service/task_service.go
@@ -3,9 +3,16 @@ package service
 import (
 	"context"
 	"errors"
+	"strings"
 
 	"github.com/Andrew1996-la/timo/internal/models"
 	"github.com/Andrew1996-la/timo/internal/repository"
+)
+
+var (
+	ErrInvalidTaskID   = errors.New("invalid task id")
+	ErrEmptyTaskTitle  = errors.New("task title cannot be empty")
+	ErrInvalidDuration = errors.New("seconds must be positive")
 )
 
 type TaskService struct {
@@ -13,44 +20,67 @@ type TaskService struct {
 }
 
 func NewTaskService(repo *repository.TaskRepository) *TaskService {
-	return &TaskService{repo: repo}
+	return &TaskService{
+		repo: repo,
+	}
 }
 
-func (t *TaskService) Create(ctx context.Context, title string) (*models.Task, error) {
+func (s *TaskService) Create(
+	ctx context.Context,
+	title string,
+) (*models.Task, error) {
+	title = strings.TrimSpace(title)
+
 	if title == "" {
-		return nil, errors.New("task title can not be empty")
+		return nil, ErrEmptyTaskTitle
 	}
 
-	return t.repo.Create(ctx, title)
+	return s.repo.Create(ctx, title)
 }
 
-func (t *TaskService) Delete(ctx context.Context, id int) error {
-	if id <= 0 {
-		return errors.New("task by id not found")
+func (s *TaskService) Delete(ctx context.Context, id int) error {
+	if err := validateTaskID(id); err != nil {
+		return err
 	}
 
-	return t.repo.Delete(ctx, id)
+	return s.repo.Delete(ctx, id)
 }
 
-func (t *TaskService) GetAll(ctx context.Context) ([]models.Task, error) {
-	return t.repo.GetAll(ctx)
+func (s *TaskService) GetAll(ctx context.Context) ([]models.Task, error) {
+	return s.repo.GetAll(ctx)
 }
 
-func (t *TaskService) GetById(ctx context.Context, id int) (*models.Task, error) {
-	if id <= 0 {
-		return nil, errors.New("task by id not found")
+func (s *TaskService) GetByID(
+	ctx context.Context,
+	id int,
+) (*models.Task, error) {
+	if err := validateTaskID(id); err != nil {
+		return nil, err
 	}
 
-	return t.repo.GetByID(ctx, id)
+	return s.repo.GetByID(ctx, id)
 }
 
-func (t *TaskService) AddTime(
+func (s *TaskService) AddTime(
 	ctx context.Context,
 	id int,
 	seconds int,
 ) error {
-	if seconds <= 0 {
-		return nil
+	if err := validateTaskID(id); err != nil {
+		return err
 	}
-	return t.repo.AddTime(ctx, id, seconds)
+
+	if seconds <= 0 {
+		return ErrInvalidDuration
+	}
+
+	return s.repo.AddTime(ctx, id, seconds)
+}
+
+func validateTaskID(id int) error {
+	if id <= 0 {
+		return ErrInvalidTaskID
+	}
+
+	return nil
 }
