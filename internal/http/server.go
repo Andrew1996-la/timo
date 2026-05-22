4ecd8c01d243387e7e1c40ef84cdbf23d099638b
Andrew <asoborev@mail.ru>
2026-05-22 23:34:00 +0300

    настройки сервера

--- internal/http/server.go
+++ internal/http/server.go
@@ -1,7 +1,15 @@
 package http
 
 import (
+	"context"
 	"net/http"
+	"time"
+)
+
+const (
+	readTimeout  = 5 * time.Second
+	writeTimeout = 10 * time.Second
+	idleTimeout  = 30 * time.Second
 )
 
 type Server struct {
@@ -11,12 +19,19 @@ type Server struct {
 func New(addr string, handler http.Handler) *Server {
 	return &Server{
 		httpServer: &http.Server{
-			Addr:    addr,
-			Handler: handler,
+			Addr:         addr,
+			Handler:      handler,
+			ReadTimeout:  readTimeout,
+			WriteTimeout: writeTimeout,
+			IdleTimeout:  idleTimeout,
 		},
 	}
 }
 
 func (s *Server) Start() error {
 	return s.httpServer.ListenAndServe()
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	return s.httpServer.Shutdown(ctx)
 }
